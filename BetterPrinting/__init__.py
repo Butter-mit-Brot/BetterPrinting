26698bf9b7868a92801d337180653b143f18d6c7
Butter-mit-Brot <83476809+Butter-mit-Brot@users.noreply.github.com>
2021-06-13 21:23:15 +0200

    Add files via upload

---
+++ BetterPrinting/__init__.py
@@ -0,0 +1,58 @@
+from termcolor import colored
+import random
+import string
+
+def oneLine(text1L):
+    print(text1L)
+
+def twoLine(text2L, text2L2):
+    print(text2L, "\n" + text2L2)
+
+def threeLine(text3L, text3L2, text3L3):
+    print(text3L, "\n" + text3L2 + "\n" + text3L3)
+
+def fourLine(text4L, text4L2, text4L3, text4L4):
+    print(text4L, "\n" + text4L2 + "\n" + text4L3 + "\n" + text4L4)
+
+def fiveLine(text5L, text5L2, text5L3, text5L4, text5L5):
+    print(text5L, "\n" + text5L2 + "\n" + text5L3 + "\n" + text5L4 + "\n" + text5L5)
+
+def color(text, color):
+    print(colored(text, color))
+
+def splitWRD(text):
+    txt = text
+    print(txt.split(" "))
+
+def randmSTR(length):
+    l = string.printable
+
+    str = ''.join(random.choice(l) for i in range(length))
+
+    print(str)
+
+def randmLTR(length):
+    l = string.ascii_letters
+
+    str = ''.join(random.choice(l) for i in range(length))
+
+    print(str)
+
+def randmDIG(length):
+    l = string.digits
+
+    str = ''.join(random.choice(l) for i in range(length))
+
+    print(str)
+
+def randmSYM(length):
+    l = string.punctuation
+
+    str = ''.join(random.choice(l) for i in range(length))
+
+    print(str)
+
+
+def creatF(name, text):
+    f = open(name, 'w')
+    f.write(text)
