3b8de64099d3137069b7fef415022794c7176095
Butter-mit-Brot <83476809+Butter-mit-Brot@users.noreply.github.com>
2021-11-22 14:50:02 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
     name='BetterPrinting',
-    version='0.2.2',
+    version='0.3.1',
     description='A all in one print function',
     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='https://github.com/Butter-mit-Brot/BetterPrinting',
@@ -18,7 +18,7 @@ setup(
     author_email='maxwbusinesspypi@gmail.com',
     license='MIT',
     classifiers=classifiers,
-    keywords=['betterprint', 'print', 'betterprinting', 'printing', 'better', 'stringmanipulation', 'instaprint'],
+    keywords=['betterprint', 'print', 'betterprinting', 'printing', 'better', 'stringmanipulation', 'instaprint', 'sysinfo', 'systeminfo'],
     packages=find_packages(),
     install_requires=['termcolor']
 )
