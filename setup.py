21b8d4d510f2ea1534a15ec138fdb759af612f19
Max Wruß <83476809+Butter-mit-Brot@users.noreply.github.com>
2023-09-22 09:24:49 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ classifiers = [
 
 setup(
     name='BetterPrinting',
-    version='0.7.3',
+    version='0.7.4',
     description='An set of printing tools',
     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='https://github.com/Butter-mit-Brot/BetterPrinting',
